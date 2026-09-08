c342c0b269c2f2ac63bfc20896775272c3864164
Abhinav Pathak <abhinavp142@gmail.com>
2026-09-08 14:11:09 +0530

    Time: 1 ms (83.65%), Space: 42.7 MB (22.48%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countCommas(int n) {
+        int count =0;
+            if(n>999){
+                count = n - 999;
+            }
+            else{
+                count =0;
+            }
+        return count;
+    }
+}
